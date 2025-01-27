5e36cbeba67b08ea763f6a0a8fa6bcd75b7457c9
Aktan <motivationaktan@gmail.com>
2025-01-27 18:37:03 +0600

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,20 @@
+import React from 'react'
+import Navbar from "./components/Navbar.jsx";
+import Hero from "./components/Hero.jsx";
+import HeadlineCards from "./components/HeadlineCards.jsx";
+import Food from "./components/Food.jsx";
+import Category from "./components/Category.jsx";
+
+function App() {
+    return (
+        <div>
+            <Navbar />
+            <Hero />
+            <HeadlineCards />
+            <Food />
+            <Category />
+        </div>
+    );
+}
+
+export default App;
